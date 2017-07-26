e89258519fcfce84a972e31273820ff1ecc840c5
alexelec <alexelecv5@gmail.com>
2017-07-26 16:01:16 +0300

    Alexelec-settings: fix create TVH logos for XtraTV

--- packages/mediacenter/Alexelec-settings/package.mk
+++ packages/mediacenter/Alexelec-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="Alexelec-settings"
-PKG_VERSION="d8f2c00"
+PKG_VERSION="c87358b"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
