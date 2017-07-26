0dafa50a58b230222e08f6c7a1792af8067d55eb
alexelec <alexelecv5@gmail.com>
2017-07-26 18:18:03 +0300

    tvheadend: bump to v4.3-302

--- packages/xmedia/tvservice/tvheadend/package.mk
+++ packages/xmedia/tvservice/tvheadend/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="tvheadend"
-PKG_VERSION="a9dffb7"
+PKG_VERSION="debe129"
 PKG_REV="3"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
