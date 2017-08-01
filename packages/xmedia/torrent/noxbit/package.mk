a8ce17753da795dcf2ee0dd0fe8b56a64345b244
alexelec <alexelecv5@gmail.com>
2017-08-01 10:38:22 +0300

    noxbit: bump to version 03.07.17

--- packages/xmedia/torrent/noxbit/package.mk
+++ packages/xmedia/torrent/noxbit/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="noxbit"
-PKG_VERSION="8142603"
+PKG_VERSION="2503772"
 PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
