213bd016894c218683353434972af2ff783efa9e
alexelec <alexelecv5@gmail.com>
2017-08-07 09:51:21 +0300

    libamcodec: bump to 6d5faf2

--- packages/multimedia/libamcodec/package.mk
+++ packages/multimedia/libamcodec/package.mk
@@ -14,8 +14,8 @@ case $TARGET_KERNEL_ARCH in
     PKG_URL="https://github.com/codesnake/libamcodec/archive/$PKG_VERSION.tar.gz"
     ;;
   arm64)
-    PKG_VERSION="bccd426"
-    PKG_URL="https://github.com/afl1/libamcodec/archive/$PKG_VERSION.tar.gz"
+    PKG_VERSION="6d5faf2"
+    PKG_URL="https://github.com/AlexELEC/libamcodec/archive/$PKG_VERSION.tar.gz"
     ;;
 esac
 PKG_DEPENDS_TARGET="toolchain alsa-lib"
