873cef8e043ddde5cef8a144e13d373eb8c81de0
alexelec <alexelecv5@gmail.com>
2017-08-13 13:51:16 +0300

    Alexelec-settings: add Grey backgrounds logo

--- packages/mediacenter/Alexelec-settings/package.mk
+++ packages/mediacenter/Alexelec-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="Alexelec-settings"
-PKG_VERSION="c188299"
+PKG_VERSION="e7cecf4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
