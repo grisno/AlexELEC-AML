0e37bf548885c381c6464056202dbde49a857feb
alexelec <alexelecv5@gmail.com>
2017-08-14 20:16:36 +0300

    S905/qca9377-firmware-aml: bump to 1.0.0-4

--- projects/S905/packages/linux-firmware/qca9377-firmware-aml/package.mk
+++ projects/S905/packages/linux-firmware/qca9377-firmware-aml/package.mk
@@ -4,18 +4,22 @@
 ################################################################################
 
 PKG_NAME="qca9377-firmware-aml"
-PKG_VERSION="1.0.0"
+PKG_VERSION="1.0.0-4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Free-to-use"
 PKG_SITE="https://boundarydevices.com/product/bd_sdmac_wifi/"
-PKG_URL="http://linode.boundarydevices.com/repos/apt/ubuntu-relx/pool/main/q/qca-firmware/qca-firmware_$PKG_VERSION.orig.tar.gz"
+PKG_URL="http://linode.boundarydevices.com/repos/apt/ubuntu-relx/pool/main/q/qca-firmware/qca-firmware_${PKG_VERSION}_armhf.deb"
 PKG_SOURCE_DIR="qca-firmware-$PKG_VERSION"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="firmware"
-
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+unpack() {
+  mkdir -p "$BUILD/$PKG_NAME-$PKG_VERSION"
+  dpkg -x "$SOURCES/$PKG_NAME/$PKG_SOURCE_NAME" "$BUILD/$PKG_NAME-$PKG_VERSION"
+}
 
 make_target() {
   : # nothing todo
@@ -23,9 +27,6 @@ make_target() {
 
 makeinstall_target() {
   mkdir -p $INSTALL/lib/firmware
-
-  cp -P LICENSE.TXT $INSTALL/lib/firmware/LICENSE.qca
-  cp -P *.bin $INSTALL/lib/firmware
-  cp -PR qca $INSTALL/lib/firmware
-  cp -PR wlan $INSTALL/lib/firmware
+    cp -PR lib/firmware $INSTALL/lib
+    cp -P usr/share/doc/qca-firmware/copyright $INSTALL/lib/firmware/LICENSE.qca
 }
