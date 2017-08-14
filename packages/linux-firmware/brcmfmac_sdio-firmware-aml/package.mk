88224e97431d5bd98f81cbeb766afb25b9f2293e
alexelec <alexelecv5@gmail.com>
2017-08-14 20:58:25 +0300

    S905: update brcmfmac_sdio-firmware-aml

--- packages/linux-firmware/brcmfmac_sdio-firmware-aml/package.mk
+++ packages/linux-firmware/brcmfmac_sdio-firmware-aml/package.mk
@@ -1,33 +1,28 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#
-#  OpenELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  OpenELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#      This file is part of Alex@ELEC - http://www.alexelec.in.ua
+#      Copyright (C) 2011-2017 Alexandr Zuyev (alex@alexelec.in.ua)
 ################################################################################
 
 PKG_NAME="brcmfmac_sdio-firmware-aml"
-PKG_VERSION="0.1"
+PKG_VERSION="33519ac"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/LibreELEC/LibreELEC.tv"
-PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_SITE="https://github.com/kszaq/brcmfmac_sdio-firmware-aml"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="firmware"
 PKG_SHORTDESC="brcmfmac_sdio-firmware: firmware for brcm bluetooth chips used in some Amlogic based devices"
 PKG_LONGDESC="Firmware for Broadcom Bluetooth devices used in some Amlogic based devices, and brcm-patchram-plus that downloads a patchram files in the HCD format to the Bluetooth based silicon and combo chips and other utility functions."
-
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
+post_makeinstall_target() {
+  cd $INSTALL/lib/firmware/brcm
+  for f in *.hcd; do
+    ln -sr $f $(grep --text -o 'BCM\S*' $f).hcd 2>/dev/null || true
+    ln -sr $f $(grep --text -o 'BCM\S*' $f | cut -c4-).hcd 2>/dev/null || true
+    ln -sr $f $(echo $f | sed -r 's/[^.]*/\U&/') 2>/dev/null || true
+    ln -sr bcm4335_V0343.0353.hcd BCM4335A0.hcd 2>/dev/null || true
+  done
+}
