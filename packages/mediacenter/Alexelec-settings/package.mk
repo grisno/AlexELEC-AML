dbdc60d95c765982068b79233051a439b1f36ab7
alexelec <alexelecv5@gmail.com>
2017-08-14 19:48:13 +0300

    Alexelec-settings: frontend.sh: fix Availink DVB

--- packages/mediacenter/Alexelec-settings/package.mk
+++ packages/mediacenter/Alexelec-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="Alexelec-settings"
-PKG_VERSION="e7cecf4"
+PKG_VERSION="2e5e937"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
