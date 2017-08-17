ffd4665619fd858174b72d67bb19e91766c14c71
alexelec <alexelecv5@gmail.com>
2017-08-17 09:31:43 +0300

    kodi: bump to 17.4-rc1-02af98c

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-GIT_VERSION="3aa19b0"
+GIT_VERSION="02af98c"
 PKG_VERSION="17.4-rc1-$GIT_VERSION"
 PKG_REV="1"
 PKG_ARCH="any"
