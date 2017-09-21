cb41884f23d09f89130cdb43e6fd724bbf0b076f
alexelec <alexelecv5@gmail.com>
2017-09-21 17:05:19 +0300

    tvheadend: bump to v4.3-488 (dvb_c_annexB.patch)

--- packages/xmedia/tvservice/tvheadend/package.mk
+++ packages/xmedia/tvservice/tvheadend/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="tvheadend"
-PKG_VERSION="f5337ad"
+PKG_VERSION="0d1cd06"
 PKG_REV="3"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
