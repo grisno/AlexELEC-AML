af15e78ba13cd8028184437dd6f5bdf50bd17ec4
alexelec <alexelecv5@gmail.com>
2017-10-04 20:19:56 +0300

    acestream-aml: rework options

--- packages/mediacenter/Alexelec-settings/package.mk
+++ packages/mediacenter/Alexelec-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="Alexelec-settings"
-PKG_VERSION="2e5e937"
+PKG_VERSION="10b322b"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
