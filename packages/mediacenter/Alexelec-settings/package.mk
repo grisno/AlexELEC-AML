e79c11eb868418b3598ffee8b5536e63c755dea4
alexelec <alexelecv5@gmail.com>
2017-11-06 16:48:19 +0200

    Alexelec-settings: bump to 30c0064

--- packages/mediacenter/Alexelec-settings/package.mk
+++ packages/mediacenter/Alexelec-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="Alexelec-settings"
-PKG_VERSION="6f22d2e"
+PKG_VERSION="30c0064"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
