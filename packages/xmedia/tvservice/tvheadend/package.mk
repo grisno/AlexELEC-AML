a27959ca9cd84fa28725eb34e755da29b6db7e80
alexelec <alexelecv5@gmail.com>
2017-11-16 10:15:28 +0200

    tvheadend: bump to c4919de

--- packages/xmedia/tvservice/tvheadend/package.mk
+++ packages/xmedia/tvservice/tvheadend/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="tvheadend"
-PKG_VERSION="a5ecd97"
+PKG_VERSION="c4919de"
 PKG_REV="3"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
