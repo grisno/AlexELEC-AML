4192853b6f7d965331ed9d06ca11449a3563c997
alexelec <alexelecv5@gmail.com>
2017-11-16 10:24:50 +0200

    oscam: bump to a44df8f; emu-patch: to b0ab93f

--- packages/xmedia/camd/oscam/package.mk
+++ packages/xmedia/camd/oscam/package.mk
@@ -4,8 +4,8 @@
 ################################################################################
 
 PKG_NAME="oscam"
-PKG_VERSION="2563c02"
-PKG_VERSION_NUMBER="11391"
+PKG_VERSION="a44df8f"
+PKG_VERSION_NUMBER="11392"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
