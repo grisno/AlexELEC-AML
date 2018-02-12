0cafe79a827ab2a35d435d8ba4e4cd1d2bc2303f
alexelec <alexelecv5@gmail.com>
2018-02-12 10:13:57 +0200

    noxbit: bump to version 03.02.18

--- packages/xmedia/torrent/noxbit/package.mk
+++ packages/xmedia/torrent/noxbit/package.mk
@@ -1,10 +1,10 @@
 ################################################################################
 #      This file is part of Alex@ELEC - http://www.alexelec.in.ua
-#      Copyright (C) 2011-2017 Alexandr Zuyev (alex@alexelec.in.ua)
+#      Copyright (C) 2011-present Alexandr Zuyev (alex@alexelec.in.ua)
 ################################################################################
 
 PKG_NAME="noxbit"
-PKG_VERSION="2503772"
+PKG_VERSION="41e4ddf"
 PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
