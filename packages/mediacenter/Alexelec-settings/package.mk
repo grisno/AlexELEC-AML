e0018f8b7307191a392d2e719df3150e29ca1dfb
alexelec <alexelecv5@gmail.com>
2018-03-02 11:26:49 +0200

    Alexelec-settings: fix syntax

--- packages/mediacenter/Alexelec-settings/package.mk
+++ packages/mediacenter/Alexelec-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="Alexelec-settings"
-PKG_VERSION="7e3e6b2"
+PKG_VERSION="a993b95"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
