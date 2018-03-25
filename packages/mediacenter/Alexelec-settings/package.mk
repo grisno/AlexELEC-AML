a83f2d5093217e902117520308dccd279831a92e
alexelec <alexelecv5@gmail.com>
2018-03-25 12:57:01 +0300

    Alexelec-settings: change update version URL

--- packages/mediacenter/Alexelec-settings/package.mk
+++ packages/mediacenter/Alexelec-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="Alexelec-settings"
-PKG_VERSION="f50065e"
+PKG_VERSION="51a2c9c"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
