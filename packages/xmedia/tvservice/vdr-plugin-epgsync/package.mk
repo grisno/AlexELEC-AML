a2bd6f82efae076f3eaf709b0850baa40228f687
alexelec <alexelecv5@gmail.com>
2018-04-19 10:35:54 +0300

    vdr-plugin-epgsync: update

--- packages/xmedia/tvservice/vdr-plugin-epgsync/package.mk
+++ packages/xmedia/tvservice/vdr-plugin-epgsync/package.mk
@@ -1,15 +1,15 @@
 ################################################################################
 #      This file is part of Alex@ELEC - http://www.alexelec.in.ua
-#      Copyright (C) 2011-2017 Alexandr Zuyev (alex@alexelec.in.ua)
+#      Copyright (C) 2011-present Alexandr Zuyev (alex@alexelec.in.ua)
 ################################################################################
 
-PKG_NAME="vdr-epgsync"
+PKG_NAME="vdr-plugin-epgsync"
 PKG_VERSION="1.0.1"
-PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://vdr.schmirler.de/"
-PKG_URL="$ALEXELEC_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="$PKG_SITE/epgsync/vdr-epgsync-$PKG_VERSION.tgz"
+PKG_SOURCE_DIR="epgsync-*"
 PKG_DEPENDS_TARGET="toolchain vdr"
 PKG_SECTION="xmedia/tvservice"
 PKG_SHORTDESC="Import the EPG of a remote VDR"
@@ -19,6 +19,9 @@ PKG_AUTORECONF="no"
 PKG_LOCALE_INSTALL="yes"
 
 make_target() {
-  VDR_DIR=$(get_build_dir vdr)
-  make VDRDIR=$VDR_DIR
+  : # none
+}
+
+makeinstall_target() {
+  make DESTDIR=$INSTALL install
 }
