fd95a40d658c7619049761ea32ac4838eeb1bda9
alexelec <alexelecv5@gmail.com>
2018-04-19 11:15:25 +0300

    vdr-plugin-svdrpservice: update

--- packages/xmedia/tvservice/vdr-plugin-svdrpservice/package.mk
+++ packages/xmedia/tvservice/vdr-plugin-svdrpservice/package.mk
@@ -1,15 +1,15 @@
 ################################################################################
 #      This file is part of Alex@ELEC - http://www.alexelec.in.ua
-#      Copyright (C) 2011-2017 Alexandr Zuyev (alex@alexelec.in.ua)
+#      Copyright (C) 2011-present Alexandr Zuyev (alex@alexelec.in.ua)
 ################################################################################
 
-PKG_NAME="vdr-svdrpservice"
+PKG_NAME="vdr-plugin-svdrpservice"
 PKG_VERSION="1.0.0"
-PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://vdr.schmirler.de/"
-PKG_URL="$ALEXELEC_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="$PKG_SITE/svdrpservice/vdr-svdrpservice-$PKG_VERSION.tgz"
+PKG_SOURCE_DIR="svdrpservice-*"
 PKG_DEPENDS_TARGET="toolchain vdr"
 PKG_SECTION="xmedia/tvservice"
 PKG_SHORTDESC="Offers SVDRP connections as a service to other plugins"
@@ -19,6 +19,9 @@ PKG_AUTORECONF="no"
 PKG_LOCALE_INSTALL="yes"
 
 make_target() {
-  VDR_DIR=$(get_build_dir vdr)
-  make VDRDIR=$VDR_DIR
+  : # none
+}
+
+makeinstall_target() {
+  make DESTDIR=$INSTALL install
 }
