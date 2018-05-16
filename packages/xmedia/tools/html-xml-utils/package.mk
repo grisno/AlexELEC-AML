384c985f40a4cda4d915ec523fc281eea085ad03
alexelec <alexelecv5@gmail.com>
2018-05-16 12:30:51 +0300

    add pkg: html-xml-utils 7.7

---
+++ packages/xmedia/tools/html-xml-utils/package.mk
@@ -0,0 +1,19 @@
+################################################################################
+#      This file is part of Alex@ELEC - http://www.alexelec.in.ua
+#      Copyright (C) 2011-present Alexandr Zuyev (alex@alexelec.in.ua)
+################################################################################
+
+PKG_NAME="html-xml-utils"
+PKG_VERSION="7.7"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://www.w3.org/Tools/HTML-XML-utils/"
+PKG_URL="$PKG_SITE/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="xmedia/tools"
+PKG_SHORTDESC="A number of simple utilities for manipulating HTML and XML files"
+PKG_LONGDESC="A number of simple utilities for manipulating HTML and XML files."
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
