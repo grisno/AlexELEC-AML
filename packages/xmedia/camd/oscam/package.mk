414ff55b243e10daac6bc3268d441c4834120e14
alexelec <alexelecv5@gmail.com>
2018-06-09 10:15:21 +0300

    oscam: bump to 11424

--- packages/xmedia/camd/oscam/package.mk
+++ packages/xmedia/camd/oscam/package.mk
@@ -4,8 +4,8 @@
 ################################################################################
 
 PKG_NAME="oscam"
-PKG_VERSION="d785952"
-PKG_VERSION_NUMBER="11420"
+PKG_VERSION="8283583"
+PKG_VERSION_NUMBER="11424"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
